f1ee371fdb43f7537f11b50882eb5479cab45f0d
gatchamonster <79342536+gatchamonster@users.noreply.github.com>
2021-10-23 22:26:06 +0100

    Map Stage 1
    added some tiles and drew the first draft of the map

---
+++ GGJ_Oct21/assets/_WORKING/Art/tileset2.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.3" name="tileset2" tilewidth="8" tileheight="8" tilecount="1080" columns="60">
+ <image source="tileset2.png" width="480" height="144"/>
+</tileset>
